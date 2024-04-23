828599c50eb1c409e622cfe7cb50f223a4716a30
Bonkahr <karanjabonface31@gmail.com>
2024-04-23 12:23:45 +0300

    add details page for a product

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("category/<int:id>", views.filter_by_category, name="filtered"),
+    path("product/<int:id>", views.product_detail, name='product-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Category, Product
+from .models import Category, Product, ProductImages
 
 
 def home(request):
@@ -16,7 +16,7 @@ def home(request):
     return render(request, "home.html", context)
 
 
-def filter_by_category(request, id: str):
+def filter_by_category(request, id: int):
     category = Category.objects.filter(id=id).first()
     categories = Category.objects.all()
     products = Product.objects.all().filter(category=category).order_by("-created_on")
@@ -24,3 +24,24 @@ def filter_by_category(request, id: str):
     context = {"products": products, "categories": categories, "category": category}
 
     return render(request, "filtered-category.html", context)
+
+
+def product_detail(request, id: int):
+    categories = Category.objects.all()
+    product = []
+    try:
+        product = get_object_or_404(Product, id=id)
+        photos = ProductImages.objects.filter(product=product)
+        context = {
+            'product': product,
+            'photos': photos,
+            'categories': categories,
+        }
+
+        return render(request, 'product-detail.html', context)
+    except:
+        context = {
+            'product': product,
+            'categories': categories,
+        }
+        return render(request, 'product-detail.html', context)
